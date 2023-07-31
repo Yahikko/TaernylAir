644e7508d32bc6fa3922adc68bca5332a688f574
Yahikko <taczuhiro@mail.ru>
2023-07-31 19:56:39 +0300

    race state, atomicfu

--- src/main/kotlin/FlightFetcher.kt
+++ src/main/kotlin/FlightFetcher.kt
@@ -1,6 +1,7 @@
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
+import kotlinx.atomicfu.atomic
 import kotlinx.coroutines.*
 
 private const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
@@ -8,15 +9,31 @@ private const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
 private const val LOYALTY_ENDPOINT = "$BASE_URL/loyalty"
 
 fun main() {
+
+    val passengersPerFlight = 75
+    val numberOfLight = 1000
+    val checkedInPassengers = atomic(0)
+
     runBlocking {
-        println("Started")
-        launch {
-            val flight = fetchFlight("Madrigal")
-            println(flight)
+        repeat(numberOfLight) {
+            launch(Dispatchers.Default) {
+                checkedInPassengers += passengersPerFlight
+            }
         }
-        println("Finished")
     }
+
+    println(checkedInPassengers)
+
+        runBlocking {
+            println("Started")
+            launch {
+                val flight = fetchFlight("Madrigal")
+                println(flight)
+            }
+            println("Finished")
+        }
 }
+
 
 suspend fun fetchFlight(passengerName: String): FlightStatus = coroutineScope {
     val client = HttpClient(CIO)
