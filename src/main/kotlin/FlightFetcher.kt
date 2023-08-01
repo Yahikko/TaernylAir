28c706d6371b603531ba9a03e5f404fbf81049bd
Yahikko <taczuhiro@mail.ru>
2023-08-01 07:50:13 +0300

    no cancellations

--- src/main/kotlin/FlightFetcher.kt
+++ src/main/kotlin/FlightFetcher.kt
@@ -2,60 +2,48 @@ import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import kotlinx.coroutines.*
-import java.util.concurrent.atomic.AtomicInteger
 
 private const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
 private const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
 private const val LOYALTY_ENDPOINT = "$BASE_URL/loyalty"
 
+private var status = ""
+
 fun main() {
 
-    val passengersPerFlight = 75
-    val numberOfLight = 5000
-    val checkedInPassengers = AtomicInteger(0)
-
     runBlocking {
-        repeat(numberOfLight) {
-            launch(Dispatchers.Default) {
-                repeat(passengersPerFlight) {
-                    checkedInPassengers.incrementAndGet()
+        launch {
+            var flight: FlightStatus
+            while (true) {
+                flight = fetchFlight("Madrigal")
+                if (status == "On Time" || status == "Delayed") {
+                    break
                 }
+                println("Searching for another flight")
             }
+            println(flight)
         }
     }
-
-    println(checkedInPassengers)
-
-    /*    runBlocking {
-            println("Started")
-            launch {
-                val flight = fetchFlight("Madrigal")
-                println(flight)
-            }
-            println("Finished")
-        }*/
 }
 
 
 suspend fun fetchFlight(passengerName: String): FlightStatus = coroutineScope {
     val client = HttpClient(CIO)
+    println("Started")
     val flightResponse = async {
-        println("Started flightResponse")
         client.get<String>(FLIGHT_ENDPOINT).also {
-            println("Finished flightResponse")
         }
     }
     val loyaltyResponse = async {
-        println("Started loyaltyResponse")
         client.get<String>(LOYALTY_ENDPOINT).also {
-            println("Finished loyaltyResponse")
         }
     }
     delay(500)
-    println("Combining flight data")
-    FlightStatus.parse(
+    val flightStatus = FlightStatus.parse(
         passengerName = passengerName,
         flightResponse = flightResponse.await(),
         loyaltyResponse = loyaltyResponse.await()
     )
+    status = flightStatus.status
+    flightStatus
 }
